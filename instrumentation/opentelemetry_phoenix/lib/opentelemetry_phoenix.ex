open-telemetry/opentelemetry-erlang-contrib

Make span names be more similar to the HTTP span names

instrumentation/opentelemetry_phoenix/lib/opentelemetry_phoenix.ex
@@ -163,11 +163,14 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.mount",
+      "live_view.mount #{target}",
       meta,
-      live_view_start_opts(meta)
+      start_opts
     )
   end
 
@@ -177,11 +180,14 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.handle_params",
+      "live_view.handle_params #{target}",
       meta,
-      live_view_start_opts(meta)
+      start_opts
     )
   end
 
@@ -191,9 +197,12 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}, event: event} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.handle_event##{event}",
+      "live_view.handle_event #{target} #{event}",
       meta,
       live_view_start_opts(meta)
     )
@@ -240,4 +249,8 @@ defmodule OpentelemetryPhoenix do
   end
 
   defp url_attributes(_meta), do: %{}
+
+  defp target(attributes, live_view) do
+    attributes[URLAttributes.url_template()] || inspect(live_view)
+  end
 end

danschultzer/opentelemetrex-erlang-contrib

instrumentation/opentelemetry_phoenix/lib/opentelemetry_phoenix.ex
@@ -166,11 +166,14 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.mount",
+      "live_view.mount #{target}",
       meta,
-      live_view_start_opts(meta)
+      start_opts
     )
   end
 
@@ -180,11 +183,14 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.handle_params",
+      "live_view.handle_params #{target}",
       meta,
-      live_view_start_opts(meta)
+      start_opts
     )
   end
 
@@ -194,9 +200,12 @@ defmodule OpentelemetryPhoenix do
         %{socket: %{view: live_view}, event: event} = meta,
         _handler_configuration
       ) do
+    start_opts = live_view_start_opts(meta)
+    target = target(start_opts.attributes, live_view)
+
     OpentelemetryTelemetry.start_telemetry_span(
       @tracer_id,
-      "#{inspect(live_view)}.handle_event##{event}",
+      "live_view.handle_event #{target} #{event}",
       meta,
       live_view_start_opts(meta)
     )
@@ -243,4 +252,8 @@ defmodule OpentelemetryPhoenix do
   end
 
   defp url_attributes(_meta), do: %{}
+
+  defp target(attributes, live_view) do
+    attributes[HTTPAttributes.http_route()] || inspect(live_view)
+  end
 end